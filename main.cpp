4a8a8bf085ec88a8771d2dc64ca3a88fb56d3fe7
Vineet Bhatawadekar <vineetvb@gmail.com>
2020-04-16 16:29:57 -0700

    shader compile and link

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -6,6 +7,26 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
+
+void processInput(GLFWwindow* window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+const GLchar* vertexShaderSource = R"END(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+void main(){
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)END";
+
+const GLchar* fragmentShaderSource = R"END(
+out vec4 FragColor;
+void main(){
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+}
+)END";
 
 int main() {
     glfwInit();
@@ -23,8 +44,10 @@ int main() {
 
     // Tell GLFW to make window to be the main context on the current thread.
     glfwMakeContextCurrent(window);
+    // Set callback for window resize.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // GLAD manages function pointers for OpenGL so we want to initialize GLAD before we call any OpenGL function:
+    // GLAD manages function pointers for OpenGL so we want to initialize GLAD before we call any OpenGL function.
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to init GLAD" << std::endl;
         return -1;
@@ -41,12 +64,52 @@ int main() {
      * http://web.cse.ohio-state.edu/~parent.1/classes/581/Lectures/4.2DviewingHandout.pdf
      */
 
-    // Set callback for window resize.
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    // Build and compile shaders.
+    // Vertex shader.
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // Check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // link shaders
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // Main Render Loop.
     while(!glfwWindowShouldClose(window)){
+        processInput(window);
+
+        // Rendering.
+        glClearColor(0.5, 0.5, 0.5, 1.0); // << state setter.
+        glClear(GL_COLOR_BUFFER_BIT);     // << state user.
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
